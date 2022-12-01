b0b38cc698dc1755d7bb9504da6cd77c4ac27635
hashim286 <112742556+hashim286@users.noreply.github.com>
2022-12-01 18:15:52 -0500

    Update day1.py

--- python/day1/day1.py
+++ python/day1/day1.py
@@ -1,1 +1,19 @@
+calorie_totals = list()
+with open("text.txt") as text:
+    total = 0
+    while True:
+        txt = text.readline()
+        if txt == "\n":
+            calorie_totals.append(total)
+            total = 0
+            continue
+        try:
+            num = int(txt)
+        except ValueError:
+            break
+        total += num
 
+
+calorie_totals = sorted(calorie_totals, reverse=True)
+print(max(calorie_totals))
+print(calorie_totals[0] + calorie_totals[1] + calorie_totals[2])
