589639888d2d3ddcbafd1485637957fa7139f502
hashim286 <hashimrauf@gmail.com>
2022-12-03 17:51:10 -0500

    Update PartTwo.java

--- java/day3/PartTwo.java
+++ java/day3/PartTwo.java
@@ -1,22 +1,40 @@
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PartTwo {
     public static void main(String[] args) {
         String path = "text.txt";
-
-
+        int total = 0;
         try (Scanner scanner = new Scanner(Paths.get(path))) {
-            int counter = 0;
+            ArrayList<Character> lettersInAll = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 String lineOne = scanner.nextLine();
                 String lineTwo = scanner.nextLine();
                 String lineThree = scanner.nextLine();
 
+                for (int i = 0, size = lineOne.length(); i < size; i++) {
+                    String letter = String.valueOf(lineOne.charAt(i));
+                    if (lineTwo.contains(letter) && lineThree.contains(letter)) {
+                        lettersInAll.add(lineOne.charAt(i));
+                        break;
+                    }
+                }
+
+            }
+            System.out.println(lettersInAll);
+            for (char letter: lettersInAll) {
+                int ascii = (int) letter;
+                if (ascii >= 97 && ascii <= 122) {
+                    total += ascii - 96;
+                } else {
+                    total += ascii - 38;
+                }
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
+        System.out.println(total);
     }
 }
