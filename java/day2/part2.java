c98366ae11c82480dda0015f403273be8a0f18d7
hashim286 <112742556+hashim286@users.noreply.github.com>
2022-12-02 23:23:39 -0500

    Update part2.java

--- java/day2/part2.java
+++ java/day2/part2.java
@@ -1,1 +1,96 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class PartTwo {
+    public static void main(String[] args) {
+        String path = "text.txt";
+        int playerScore = 0;
+        ArrayList<String> text = readFile(path);
+        for (int i = 0, length = text.size(); i < length; i++) {
+            String[] moves = text.get(i).split(" ");
+            int opponentMove = translateMoveToNumber(moves[0]);
+            int yourMove = determineComplementMove(opponentMove, moves[1]);
+            playerScore += determineWinner(opponentMove, yourMove) + yourMove;
+        }
+
+        System.out.println(playerScore);
+    }
+
+    public static ArrayList<String> readFile(String path) {
+        ArrayList<String> input = new ArrayList<>();
+
+        try(Scanner scanner = new Scanner(Paths.get(path))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                input.add(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return input;
+    }
+
+    public static int translateMoveToNumber(String move) {
+
+        int pointsForRock = 1;
+        int pointsForPaper = 2;
+        int pointsForScissors = 3;
+
+        if (move.equals("A")) {
+            return pointsForRock;
+        } else if (move.equals("B")) {
+            return pointsForPaper;
+        } else {
+            return pointsForScissors;
+        }
+    }
+
+    public static int determineComplementMove(int move, String condition) {
+
+        int pointsForRock = 1;
+        int pointsForPaper = 2;
+        int pointsForScissors = 3;
+
+        if (condition.equals("X")) {
+            move -= 1;
+            if (move == 0) {
+                return pointsForScissors;
+
+            } else if (move == 1) {
+                return pointsForRock;
+            }
+            else {
+                return pointsForPaper;
+            }
+        } else if (condition.equals ("Y")) {
+            return move;
+        }
+
+        else {
+            move -= 1;
+            if (move == 0)
+                return pointsForPaper;
+            else if (move == 1) {
+                return pointsForScissors;
+            }
+            else {
+                return pointsForRock;
+            }
+        }
+    }
+
+    public static int determineWinner(int moveOne, int moveTwo) {
+        int pointsForWin = 6;
+        int pointsForDraw = 3;
+        int pointsForLoss = 0;
+
+        if (moveTwo - moveOne == 1 || moveOne - moveTwo == 2) {
+            return pointsForWin;
+        } else if (moveTwo == moveOne) {
+            return pointsForDraw;
+        } else {
+            return pointsForLoss;
+        }
+    }
+}
